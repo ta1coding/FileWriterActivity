befc91d9f726109eecaa74c33fe7d20f18340adf
Tyler <tanderson1@hwemail.com>
2024-09-08 22:51:56 -0700

    Added hidden & visible files; Made .gitignore

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -4,46 +4,42 @@ import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
     public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
+        String secretName = "Barack";
+        String secretCode = "8241763509";
 
         // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // try (FileWriter writer = new FileWriter(fileName1)) {
+        //     writer.write(data);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
         // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".Obama's Last Name.txt"))) {
+            bufferedWriter.write(secretName);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
+        //     outputStream.write(data.getBytes());
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
         // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
+        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(".secretInfo/Theiss Bank Account Number.dat"))) {
+            bufferedOutputStream.write(secretCode.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    //     try {
+    //         Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
     }
 }
